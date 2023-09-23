3c4470e806138ee6fd5e5053dfacbb29914eb444
diasbouk <boukdsaid@gmail.com>
2023-09-23 23:24:15 +0100

    Code Improved a lil bit

--- main.js
+++ main.js
@@ -13,42 +13,85 @@
     }
 }
 let computerSelection = getComputerChoice();
-let playerSelection = 'rock' //window.prompt("pick your move");
+let AiScore = document.querySelector('#AiScore').innerHTML;
+AiScore = Number(AiScore);
+let humanScore = document.querySelector('#humanScore').innerHTML;
+humanScore = Number(humanScore);
+let playerSelection; //window.prompt("pick your move");
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    playRound();
+    console.log(playRound(playerSelection,computerSelection));
+})
+let paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    playRound();
+    switch (humanWin) {
+        case 0:
+            AiScore++;
+            break;
+            case 1:
+                humanScore++;
+                case 2:
+                    break
+        default:
+            break;
+    }
+    console.log(playRound(playerSelection,computerSelection));
+})
+let scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    playRound();
+    console.log(playRound(playerSelection,computerSelection));
+})
 
+let humanWin;
 
     function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock') {
         if (computerSelection == 'rock') {
+            humanWin = 2;
             return 'Tie';
         }
         else if (computerSelection == 'paper') {
+            humanWin = 0;
             return 'You lost !';
         }
         else if (computerSelection == 'scissors') {
+            humanWin = 1;
             return 'You won !!';
         }
         else return 'Error';
     }
     else if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
+            humanWin = 1;
             return 'You won';
         }
         else if (computerSelection == 'paper') {
+            humanWin = 2;
             return 'Tie';
         }
         else if (computerSelection == 'scissors') {
+            humanWin = 0;
             return 'You Lost!!';
         }
     }
 
     else if (playerSelection == 'scissors') {
         if (computerSelection == 'rock') {
+            humanWin = 0;
             return 'You lost';
         }
         else if (computerSelection == 'paper') {
+            humanWin = 1;
             return 'You won';
         }
         else if (computerSelection == 'scissors') {
+            humanWin = 2;
             return 'Tie';
         }
     }
